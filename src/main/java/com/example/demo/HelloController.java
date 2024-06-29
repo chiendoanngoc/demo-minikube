b0547d9f4461861a8e16b23b4332d54c5e7bbd4f
chiendoanngoc <chiendoanngoc1705@gmail.com>
2024-06-30 02:00:50 +0700

    2nd app

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -14,8 +14,9 @@ public class HelloController {
     @GetMapping("/second-app")
     public String secondAppSayHello() {
         final String uri = "http://localhost:60464/hello";
+        final String url = "http://service-1st-app.1st-app-namespace.svc.cluster.local/hello";
         RestTemplate restTemplate = new RestTemplate();
-        String firstAppResponse = restTemplate.getForObject(uri, String.class);
+        String firstAppResponse = restTemplate.getForObject(url, String.class);
         return firstAppResponse + "That is the greeting from 1st app. And I'm the 2nd app!";
     }
 }
